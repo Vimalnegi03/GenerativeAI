89b632d956d033d9b15f6bd0ec48553ae846eec1
Vimal <vm.21cs48@thdcihet.ac.in>
2025-07-10 12:08:40 +0530

    added checkpointing which serves as memory

--- lang_graph/graph2.py
+++ lang_graph/graph2.py
@@ -1,10 +1,13 @@
 from typing import Annotated
 from langchain.chat_models import init_chat_model
 from typing_extensions import TypedDict
-
+from langgraph.checkpoint.mongodb import MongoDBSaver
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+#to pause flow by ai (tool is decorator over here) 
+from langchain_core.tools import tool
 llm=init_chat_model(model_provider="openai",model="gpt-4.1")
+MONGODB_URI="mongodb+srv://@cluster0.up4jy8m.mongodb.net"
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
     # in the annotation defines how this state key should be updated
@@ -20,5 +23,11 @@ graph_builder.add_node("chatbot",chatbot)
 graph_builder.add_edge(START,"chatbot")
 graph_builder.add_edge("chatbot",END)
 
+#without any memory
 graph=graph_builder.compile()
+#human in loop basically when we want to interrupt execution for example you want to comfirm something then it is used
+# graph.interrupt_after_nodes("confirm_transaction")
 
+#basically adds memory
+def create_chat_graph(checkpointer):
+    return graph_builder.compile(checkpointer=checkpointer)
--- lang_graph/hello.py
+++ lang_graph/hello.py
@@ -1,12 +1,16 @@
 from graph2 import graph
-
-
+from graph2 import create_chat_graph
+from langgraph.checkpoint.mongodb import MongoDBSaver
+MONGODB_URI="mongodb+srv://@cluster0.up4jy8m.mongodb.net"
+config={"configurable":{"thread_id":"1"}}
 def init():
-    while True:
-        user_input=input("> ")
-        for event in graph.stream({"messages":[{"role":"user","content":user_input}]},stream_mode="values"):
-            if "messages" in event:
-                event["messages"][-1].pretty_print()
+    with MongoDBSaver.from_conn_string(MONGODB_URI) as checkpointer:
+        graph_with_mongo=create_chat_graph(checkpointer=checkpointer)
+        while True:
+            user_input=input("> ")
+            for event in graph_with_mongo.stream({"messages":[{"role":"user","content":user_input}]},config,stream_mode="values"):
+                if "messages" in event:
+                    event["messages"][-1].pretty_print()
 
 
 init()        
